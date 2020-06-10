66a695a13821ce7cfb46cc2bf99af28848332db8
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-10 16:05:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Partie 8 exercice1</title>
+</head>
+<body>
+    <p><?= 'User Agent : ' . $_SERVER['HTTP_USER_AGENT']; ?></p>
+    <p><?= 'Adresse IP : ' . $_SERVER['SERVER_ADDR']; ?></p>
+    <p><?= 'Nom du serveur : ' . $_SERVER['SERVER_NAME']; ?></p>
+</body>
+</html>
